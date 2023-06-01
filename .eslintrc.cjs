621c718e638aefefbfa7fd5c36237bcf9b432fc1
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2023-06-01 08:22:40 +0200

    style:#19 added eslint config airbnb to branch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,39 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'airbnb-base',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:import/typescript',
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  "rules": {
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "warn",
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+    "import/no-unresolved": 0,
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+    ]
+  },
+  "plugins": ["@typescript-eslint"],
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
 }
