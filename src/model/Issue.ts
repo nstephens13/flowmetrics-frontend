a96c6a4c16ccf0dcc8185aaeeb067539fd79f0b2
ceggers <clara.eggers@stud.htwk-leipzig.de>
2023-06-07 22:08:21 +0200

    feat:#20 IssueIF new enum and nullable

--- src/model/Issue.ts
+++ src/model/Issue.ts
@@ -8,12 +8,25 @@ import type { Employee } from './Employee';
  * @prop {Employee} assignedTo the employee that is assigned to the issue
  * @prop {Date} createdAt the Date when the issue was created
  * @prop {Date} closedAt the Date when the issue was closed
+ * @prop {status} status status of ticket-progress
  */
+
+enum status{
+  Open,
+  Closed,
+  InWork,
+  Resolved,
+  // Zombie
+}
+
 export interface Issue {
+
   id: number;
   name: string;
-  description: string;
-  assignedTo: Employee;
+  description: string| null;
+  assignedTo: Employee| null;
+  createdBy: Employee;
   createdAt: Date;
-  closedAt: Date;
+  closedAt: Date| null;
+  status: status;
 }
