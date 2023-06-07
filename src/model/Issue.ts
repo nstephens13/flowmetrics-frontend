7c96c71d8701924e8744f807b6723c5326368524
ceggers <clara.eggers@stud.htwk-leipzig.de>
2023-06-07 22:09:20 +0200

    docs:#20 missing createdBy

--- src/model/Issue.ts
+++ src/model/Issue.ts
@@ -6,6 +6,7 @@ import type { Employee } from './Employee';
  * @prop {string} name the name of the issue
  * @prop {string} description the description of the issue
  * @prop {Employee} assignedTo the employee that is assigned to the issue
+ * @prop {Employee} createdBy the employee that created the issue
  * @prop {Date} createdAt the Date when the issue was created
  * @prop {Date} closedAt the Date when the issue was closed
  * @prop {status} status status of ticket-progress
