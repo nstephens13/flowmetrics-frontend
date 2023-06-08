409fddb59725954c068f2eefe27b1bec3e573a7f
Estelle Wilfer <estelle_bianca.wilfer@stud.htwk-leipzig.de>
2023-06-08 14:01:54 +0200

    feat:#23 initial commit

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/employee overview">Employee Overviev</RouterLink>
       </nav>
     </div>
   </header>
