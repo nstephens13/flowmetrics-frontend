54ebd8478b78276ba5a04bf7008a3c417af9429d
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2023-06-08 16:53:48 +0200

    feat:#25 initial commit

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,51 @@
+
+<template>
+        <Menubar :model="items" label="FM" class="mr-2" size="large">
+            <template #start >
+                <h2 id="productName">FlowMetrics</h2>
+            </template>
+            <template #end>
+                <Button rounded text icon="pi pi-ellipsis-v" style="color: var(--primary-color-text)" aria-label="Submit"></Button>
+            </template>
+        </Menubar>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const items = ref([
+    {
+        label: 'Project Overview',
+        icon: 'pi pi-fw pi-file',
+        to: '/about'
+    },
+    {
+        label: 'Employee Overview',
+        icon: 'pi pi-fw pi-pencil',
+        to: '/'
+    },
+    {
+        label: 'New Rule',
+        icon: 'pi pi-fw pi-user',
+    }
+]);
+</script>
+
+<style scoped>
+.p-menubar{
+     background-color:#2196f3; 
+     color: #ffffff;
+     border: 0;
+     border-radius: 0%;
+}
+
+#productName{
+    margin: 1rem;
+}
+
+#options{ 
+     color: #ffffff;
+     border: 0;
+     border-radius: 50%;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
 import Calendar from 'primevue/calendar';
+import Menubar from 'primevue/menubar';
+import Avatar from 'primevue/avatar';
+import InputText from 'primevue/inputtext';
 import router from './router';
 import App from './App.vue';
 import 'primevue/resources/themes/lara-light-indigo/theme.css';
 import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 app.use(PrimeVue);
@@ -15,4 +19,7 @@ app.use(router);
 
 app.component('Calendar', Calendar);
 app.component('Button', Button);
+app.component('Menubar', Menubar);
+app.component('Avatar', Avatar);
+app.component('InputText', InputText);
 app.mount('#app');
