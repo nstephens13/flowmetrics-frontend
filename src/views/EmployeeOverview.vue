3ef55464867796cfb04a93d2ed95b00668aba661
Estelle Wilfer <estelle_bianca.wilfer@stud.htwk-leipzig.de>
2023-06-09 13:14:03 +0200

    feat:#23 added user name which is fixed to its background

--- src/views/EmployeeOverview.vue
+++ src/views/EmployeeOverview.vue
@@ -4,8 +4,15 @@
 
     <template #content>
       <div class="icon-container">
-        <div class="icon-background"></div>
-        <span class="pi pi-user user-size"></span>
+        <div class="icon-background">
+          <span class="pi pi-user user-size"></span>
+        </div>
+      </div>
+
+      <div class="user-name-container">
+        <div class="user-name-background">
+          <div class="user-name">John Doe</div>
+        </div>
       </div>
     </template>
   </Card>
@@ -26,6 +33,7 @@ export default defineComponent({
 </script>
 
 <style>
+/* Card Component */
 .background-card {
   width: 1440px; /* Set the width to match the MacBook Air 13'' screen width */
   height: 700px;
@@ -33,6 +41,8 @@ export default defineComponent({
 .card-title {
   font-size: 20px;
 }
+
+/* Employee icon and background */
 .icon-container {
   position: relative;
   top: 20px;
@@ -42,13 +52,35 @@ export default defineComponent({
   position: absolute;
   width: 100px;
   height: 100px;
-  background-color: rgba(128, 128, 128, 0.5); /* RGB values and opacity */
-  border-radius: 10%; /* Change roundness of edges */
+  background-color: rgba(128, 128, 128, 0.7); /* RGB values and opacity */
+  border-radius: 5%; /* Change roundness of edges */
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .user-size {
   font-size: 70px;
   color: white;
+}
+/* User name and background */
+.user-name-container {
+  position: relative;
+  top: 130px;
+  left: 30px;
+}
+.user-name-background {
   position: absolute;
-  transform: translate(21%, 20%); /* Center icon in icon-background */
+  width: 100px;
+  height: 20px;
+  background-color: rgba(45, 108, 193, 0.9);
+  border-radius: 5%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.user-name {
+  font-size: 13px;
+  color: white;
+  position: absolute;
 }
 </style>
