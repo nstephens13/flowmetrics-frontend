187b57fd30751d6bd2722dad1e6435a9ca7fba30
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2023-06-10 13:11:34 +0200

    fix:#27 fixed unreachable mock data, moved mock data to assets, dont use node filesystem anymore

--- src/services/workloadCalculator.ts
+++ src/services/workloadCalculator.ts
@@ -4,7 +4,7 @@ import type { IssueIF } from '@/model/IssueIF';
 import { Status } from '../model/IssueIF';
 
 // just temporary import
-import { getMockData } from './__mockdata__/mockDataComposer';
+import { getMockData } from '../assets/__mockdata__/mockDataComposer';
 
 /**
  * This function calculate the workload from a project team, and give the
@@ -12,11 +12,12 @@ import { getMockData } from './__mockdata__/mockDataComposer';
  * the amount of assigned Issues that are open but not closed or in progress, the
  * amount of Issues that are in progress, and the amount that are closed
  *
- * @param project Project Object that should be calculated
+ * @param project Project Object that should be calculated, if null a project
+ * with random mock data will be used
  * @returns {Map} key:Employee, value:{ openIssues: number; inProgressIssues: number; closedIssues: number }
  */
 function calculateWorkload(
-  project: ProjectIF,
+  project: ProjectIF | null,
 ): Map<EmployeeIF, { openIssues: number; inProgressIssues: number; closedIssues: number }> {
   const mapToReturn:Map<EmployeeIF, { openIssues: number; inProgressIssues: number; closedIssues: number }> = new Map([]);
   const issueSet: Set<IssueIF> = new Set<IssueIF>();
