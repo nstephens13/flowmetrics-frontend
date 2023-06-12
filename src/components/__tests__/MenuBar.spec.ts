5437257aed8ffd22965f03b7296153186f87e096
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2023-06-12 22:53:43 +0200

    test:#25 fixed test

--- src/components/__tests__/MenuBar.spec.ts
+++ src/components/__tests__/MenuBar.spec.ts
@@ -1,27 +1,35 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import Button from 'primevue/button';
 import Sidebar from 'primevue/sidebar';
 import Menu from 'primevue/menu';
 import PrimeVue from 'primevue/config';
+import Menubar from 'primevue/menubar';
+import router from '@/router';
 
-import { mount } from '@vue/test-utils';
+import { VueWrapper, mount } from '@vue/test-utils';
 import MenuBar from '../MenuBar.vue';
 
-const wrapper = mount(MenuBar, {
-  props: {  },
-  global: {
-    plugins: [PrimeVue],
-    components: { Button, Sidebar, Menu },
-  },
-});
+describe('Menubar Button should open sidebar', () => {
+  test('should open sidebar', async () => {
+    const wrapper: VueWrapper = mount(MenuBar, {
+      global: {
+        plugins: [PrimeVue, router],
+        components: { Button, Sidebar, Menu, Menubar },
+      },
+    });
 
-describe('Button sidebarButton', () => {
-  // Test if the button is clickable
-  it('should be clickable', async () => {
-    expect(wrapper.getComponent('Button').attributes().id).toBe("sidebarButton");
+    let menu = wrapper.findComponent(Menu);
+    expect(menu.exists()).toBe(false);
 
-    // Assert that the button is clickable (e.g., check if a specific behavior happens when clicked)
-    // You can add your own assertions based on your component's expected behavior
-    // expect('button is clickable').toBeTruthy();
+    const button = wrapper.findComponent(Button);
+    await button.trigger('click');
+    let sidebar = wrapper.findComponent(Sidebar);
+    expect(sidebar.exists()).toBe(true);
+
+    menu = wrapper.findComponent(Menu);
+    expect(menu.exists()).toBe(true);
+
+    await button.trigger('click');
+    expect(sidebar.isVisible()).toBe(false);
   });
 });
