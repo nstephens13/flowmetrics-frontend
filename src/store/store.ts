3c96f9944dc3a2c7b413d80da2e94893f9259574
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2023-06-12 11:06:30 +0200

    fix:#6 preconfigured deadlines working now

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,9 +11,15 @@ const useSLAStore = defineStore('sla', {
       { id: 3, name: 'Category 3', description: 'Description 3' },
     ] as SLACategory[],
     deadlines: [
-      { categoryId: 1, durationInDays: 3, expirationDate: new Date('2023-07-15') },
-      { categoryId: 2, durationInDays: 5, expirationDate: new Date('2023-07-17') },
-      { categoryId: 3, durationInDays: 7, expirationDate: new Date('2023-07-19') },
+      {
+        id: 1, name: 'Deadline 1', durationInDays: 3, expirationDate: new Date('2023-07-15'),
+      },
+      {
+        id: 2, name: 'Deadline 2', durationInDays: 5, expirationDate: new Date('2023-07-17'),
+      },
+      {
+        id: 3, name: 'Deadline 3', durationInDays: 7, expirationDate: new Date('2023-07-19'),
+      },
     ] as SLADeadline[],
     slaRules: [] as SLARule[],
 
