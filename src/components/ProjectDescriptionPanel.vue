5c8ef7018cc24388440f60babf377bfc94ccabc0
Elmar Kresse <elmar.kresse@stud.htwk-leipzig.de>
2023-06-21 00:39:59 +0200

    feat:#20 added circular progress bar

--- src/components/ProjectDescriptionPanel.vue
+++ src/components/ProjectDescriptionPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card" style="position: relative;">
+  <div class="card" style="position: relative">
     <Card>
       <template #title>
         Project Overview
@@ -9,17 +9,50 @@
         <Panel>
           <template #icons>
             <Dropdown
-            v-model="selectedProject"
-            :options="projects"
-            optionLabel="name"
-            placeholder="Select a project"
-            class="w-full md:w-14rem"
+              v-model="selectedProject"
+              :options="projects"
+              optionLabel="name"
+              placeholder="Select a project"
+              class="w-full md:w-14rem"
             />
           </template>
           <template #default>
-            Project-ID: {{ selectedProject.id }}<br>
-            Description: {{ selectedProject.description }}<br>
-            Total issues : {{ selectedProject.issues.length }}<br>
+            <div class="flex flex-row flex-wrap card-container justify-content-left">
+              <h4 class="m-2">Project-ID: {{ selectedProject.id }}</h4>
+              <h4 class="m-2">Description: {{ selectedProject.description }}</h4>
+              <h4 class="m-2">Total issues : {{ selectedProject.issues.length }}</h4>
+            </div>
+            <div class="card">
+              <div class="flex flex-row flex-wrap card-container justify-content-center">
+                <div class="flex-wrap flex align-items-center justify-content-center">
+                  <CircularProgressBar
+                    class="flex align-items-center justify-content-center m-2"
+                    :value="countIssuesByStatus(selectedProject.issues, Status.Open)"
+                    :max="getIssueCountMax(selectedProject.issues)"
+                    percentage
+                    rounded
+                    title="Open Tickets"
+                  />
+                  <CircularProgressBar
+                    class="flex align-items-center justify-content-center m-2"
+                    :value="countIssuesByStatus(selectedProject.issues, Status.Closed)"
+                    :max="getIssueCountMax(selectedProject.issues)"
+                    percentage
+                    rounded
+                    title="Closed Tickets"
+                  />
+                  <CircularProgressBar
+                    class="flex align-items-center justify-content-center m-2"
+                    :value="countIssuesByStatus(selectedProject.issues, Status.InProgress)"
+                    :max="getIssueCountMax(selectedProject.issues)"
+                    percentage
+                    rounded
+                    title="In Progress"
+                  />
+                </div>
+                <div class="flex-grow-1 flex align-items-center justify-content-center"></div>
+              </div>
+            </div>
           </template>
         </Panel>
       </template>
@@ -33,7 +66,7 @@
       </template>
       <template #content>
         <div class="card">
-          <DataTable :value="selectedProjectIssues" showGridlines>
+          <DataTable :value="selectedProject.issues" showGridlines>
             <Column field="id" header="Ticket-ID"></Column>
             <Column field="name" header="Name"></Column>
             <Column field="description" header="Description"></Column>
@@ -55,12 +88,14 @@
 </template>
 
 <script setup lang="ts">
-import { defineComponent } from 'vue';
-import type { ProjectIF } from '@/model/ProjectIF';
-
-import getMockData from '../assets/__mockdata__/mockDataComposer';
-import type { IssueIF } from '@/model/IssueIF';
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+import CircularProgressBar from '@/components/TickerCalculator/CircularProgressBar.vue';
 import type { EmployeeIF } from '@/model/EmployeeIF';
+import type { ProjectIF } from '@/model/ProjectIF';
+import getMockData from '@/assets/__mockdata__/mockDataComposer';
+import { countIssuesByStatus, Issue } from '@/model/Issue';
+import { Status } from '@/model/IssueIF';
 
 function printAssignedTo(employee: EmployeeIF | null): string {
   const firstName = employee?.firstName ?? '';
@@ -69,41 +104,34 @@ function printAssignedTo(employee: EmployeeIF | null): string {
 }
 
 defineExpose({ printAssignedTo });
-</script>
 
-<script lang="ts">
+const selectedProject = ref({
+  id: 0,
+  name: 'Project_Name',
+  description: '',
+  milestones: [],
+  issues: [],
+} as ProjectIF);
 
-export default defineComponent({
-  name: 'ProjectDescriptionPanel',
-  data() {
-    return {
-      selectedProject: {
-        id: 0,
-        name: 'Project_Name',
-        description: '',
-        milestones: [],
-        issues: [],
-      } as ProjectIF,
-      projects: [
-        getMockData(1),
-        getMockData(2),
-        getMockData(3),
-        getMockData(53),
-        getMockData(54),
-        getMockData(55),
-      ] as ProjectIF[],
-    };
-  },
-  computed: {
-    selectedProjectIssues(): IssueIF[] {
-      return this.selectedProject.issues;
-    },
-  },
-});
+const projects: Ref<ProjectIF[]> = ref([
+  getMockData(1),
+  getMockData(2),
+  getMockData(3),
+  getMockData(53),
+  getMockData(54),
+  getMockData(55),
+] as ProjectIF[]);
+
+function getIssueCountMax(issues: Issue[]): number {
+  if (issues.length === 0) {
+    return 100;
+  }
+  return issues.length;
+}
 </script>
 
 <style scoped>
-.p-card{
+.p-card {
   margin: 15px;
   box-shadow: none;
 }
