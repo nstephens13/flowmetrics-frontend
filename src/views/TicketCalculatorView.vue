39f34dc2d6a7bcc1fb76741d022612811db39cf6
Elmar Kresse <elmar.kresse@stud.htwk-leipzig.de>
2023-06-23 18:51:47 +0200

    feat:#20 update tests and packages

--- src/views/TicketCalculatorView.vue
+++ src/views/TicketCalculatorView.vue
@@ -6,12 +6,12 @@
         <Divider></Divider>
       </template>
       <template #content>
-        <TicketCalculatorTopPanel />
+        <TicketCalculator></TicketCalculator>
       </template>
     </Card>
   </div>
 </template>
 
 <script setup lang="ts">
-import TicketCalculatorTopPanel from '@/components/TicketCalculatorTopPanel.vue';
+import TicketCalculator from '@/components/TicketCalculator.vue';
 </script>
