ff5a94b9532e79fdd9bd7efd86cd153a6ada20c1
Ingmar Eckhardt <ingmar.eckhardt@stud.htwk-leipzig.de>
2023-06-29 23:44:22 +0200

    test:#36 added test

--- src/views/__tests__/EmployeeOverview.spec.ts
+++ src/views/__tests__/EmployeeOverview.spec.ts
@@ -1,9 +1,16 @@
 import { describe, test, expect } from 'vitest';
-import { mount } from '@vue/test-utils';
+import { mount, VueWrapper } from '@vue/test-utils';
 import PrimeVue from 'primevue/config';
 import Card from 'primevue/card';
 import { createPinia } from 'pinia';
+
 import EmployeeOverview from '../EmployeeOverview.vue';
+import {
+  devStatusList,
+  nonDisplayedStatusList,
+  planningStatusList,
+  testingStatusList,
+} from '../../assets/__mockdata__/mockDataComposer';
 
 const pinia = createPinia();
 describe('EmployeeOverview with a simple manual constructed map should render correctly', () => {
@@ -13,6 +20,10 @@ describe('EmployeeOverview with a simple manual constructed map should render co
       plugins: [PrimeVue, pinia],
       components: {
         Card,
+      },
+
+      stubs: {
+        teleport: false,
       },
     },
   });
@@ -25,8 +36,6 @@ describe('EmployeeOverview with a simple manual constructed map should render co
     expect(wrapper.text()).toContain('JOHN DOE');
   });
 
-  const result = wrapper.text();
-
   test('all Boxes exists exactly one time', () => {
     expect(openBoxes[0].exists()).toBe(true);
     expect(inProgressBoxes[0].exists()).toBe(true);
@@ -34,5 +43,44 @@ describe('EmployeeOverview with a simple manual constructed map should render co
     expect(openBoxes.length).toBe(13);
     expect(inProgressBoxes.length).toBe(13);
     expect(closedBoxes.length).toBe(13);
+  });
+});
+
+describe('EmployeeOverview with a simple manual constructed map should render correctly', () => {
+  const wrapper: VueWrapper = mount(EmployeeOverview, {
+    global: {
+      plugins: [PrimeVue, pinia],
+      components: {
+        Card,
+      },
+
+      stubs: {
+        teleport: false,
+      },
+    },
+  });
+
+  test('it mounts', () => {
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find('.dropdown-container').isVisible()).toBe(true);
+    expect(wrapper.isVisible()).toBe(true);
+  });
+  test('all strings in the filter dropdown are rendered', async () => {
+    // Open the dropdown
+    const dropdownButton = wrapper.find('.dropdown-button');
+    await dropdownButton.trigger('click');
+
+    // Get all the options in the dropdown
+    const dropdownOptions = wrapper.findAll('.dropdown-menu li');
+
+    // Extract the text of each option
+    const optionTexts = dropdownOptions.map((option) => option.text());
+
+    // Define the expected options based on the mock data
+    const expectedOptions = [...planningStatusList,
+      ...devStatusList, ...testingStatusList, ...nonDisplayedStatusList];
+
+    // Assert that the option texts match the expected options
+    expect(optionTexts).toEqual(expectedOptions);
   });
 });
