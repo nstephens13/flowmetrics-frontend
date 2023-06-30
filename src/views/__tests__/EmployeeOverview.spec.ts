61060756f76b108d06c56c7caa2e184062cf4299
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2023-06-30 09:38:24 +0200

    fix:#47 refractor test for Employee Overview

--- src/views/__tests__/EmployeeOverview.spec.ts
+++ src/views/__tests__/EmployeeOverview.spec.ts
@@ -1,4 +1,4 @@
-import {mount} from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
 import PrimeVue from 'primevue/config'
 import router from '@/router/index'
@@ -11,36 +11,33 @@ import Dropdown from 'primevue/dropdown'
 import EmployeeOverview from '../EmployeeOverview.vue'
 
 describe('Employee Overview should load all the Components', () => {
-    const wrapper = mount(EmployeeOverview, {
-        global: {
-            plugins: [PrimeVue, router],
-            components: {
-                Card,
-                DataView,
-                Divider,
-                Dropdown
-            }
-        }
-    }) 
-    test('it mounts', () => {
-        expect(wrapper.exists()).toBe(true)
-        expect(wrapper.getComponent(Card).isVisible()).toBe(true)
-        expect(wrapper.getComponent(DataView).isVisible()).toBe(true)
-        expect(wrapper.getComponent(Divider).isVisible()).toBe(true)
-        expect(wrapper.getComponent(Dropdown).isVisible()).toBe(true)
-    })  
-    
-    test('Dropdown select should be shown and in English', () => {
-        expect(wrapper.getComponent(Dropdown).props('placeholder')).toBe('Select a project')
-    })
-    
-    test('Dropdown Selection should contain all projects', () => {
-        wrapper
-          .getComponent(Dropdown)
-          .trigger('click')
-          .then(() => {
-            const dropdownOptions = wrapper.getComponent(Dropdown).props('options')
-            expect(dropdownOptions.length).toEqual(6)
-          })
-      })
+  const wrapper = mount(EmployeeOverview, {
+    global: {
+      plugins: [PrimeVue, router],
+      components: {
+        Card,
+        DataView,
+        Divider,
+        Dropdown
+      }
+    }
+  })
+
+  test('it mounts', () => {
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.findComponent(Card).isVisible()).toBe(true)
+    expect(wrapper.findComponent(DataView).isVisible()).toBe(true)
+    expect(wrapper.findComponent(Divider).isVisible()).toBe(true)
+    expect(wrapper.findComponent(Dropdown).isVisible()).toBe(true)
+  });
+
+  test('Dropdown select should be shown and in English', () => {
+    const dropdownPlaceholder = wrapper.findComponent(Dropdown).props('placeholder')
+    expect(dropdownPlaceholder).toBe('Select a project')
+  });
+
+  test('Dropdown Selection should contain all projects', async () => {
+    const dropdownOptions = wrapper.findComponent(Dropdown).props('options')
+    expect(dropdownOptions.length).toEqual(6)
+  });
 })
