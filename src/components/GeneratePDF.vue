9f72731e2bbb018f56811b763ceb887c582acd31
Estelle Wilfer <estelle_bianca.wilfer@stud.htwk-leipzig.de>
2023-07-01 15:07:14 +0200

    feat:#37 added props and SLARule.json

--- src/components/GeneratePDF.vue
+++ src/components/GeneratePDF.vue
@@ -6,12 +6,26 @@
 import Button from 'primevue/button';
 import jsPDF from 'jspdf';
 
+const props = defineProps({
+  slaRule: {
+    type: String,
+    required: false,
+    default: '',
+  },
+  issue: {
+    type: String,
+    required: false,
+    default: '',
+  },
+});
+
 const generatePDF = () => {
   // Create a new PDF
   const doc = new jsPDF();
   // Document title
   doc.setFontSize(24);
   doc.text('SLA Rule Report', 10, 15);
+  doc.text('SLA Rule', 10, 25);
   doc.save('SLARuleReport.pdf');
 };
 </script>
