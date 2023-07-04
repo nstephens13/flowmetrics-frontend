913651ba8ef93d5458773e461ea82488189a274e
Elmar Kresse <elmar.kresse@stud.htwk-leipzig.de>
2023-07-04 22:46:16 +0200

    test:#49 added tests for Employee Overview

---
+++ src/store/__tests__/projectStore.spec.ts
@@ -0,0 +1,120 @@
+import { describe, beforeEach, afterEach, expect, test } from 'vitest';
+import { createPinia, setActivePinia } from 'pinia';
+import useProjectsStore from '@/store/ProjectStore';
+
+describe('useProjectsStore', () => {
+  let store: ReturnType<typeof useProjectsStore>;
+
+  beforeEach(() => {
+    const pinia = createPinia();
+    setActivePinia(pinia);
+    store = useProjectsStore();
+  });
+
+  afterEach(() => {
+    store.$reset();
+  });
+
+  test('should add a project to the store', () => {
+    const project = {
+        id: 1,
+        name: 'Project 1',
+        description: 'Project 1 description',
+        milestones: [],
+        issues: [
+          { id: 1,
+            status: 'Open',
+            name: 'Issue 1',
+            description: 'Issue 1 description',
+            assignedTo: {id: 4, firstName: "Bernd", lastName: "Mustermann"},
+            createdBy: {id: 4, firstName: "Bernd", lastName: "Mustermann"},
+            dueTo: new Date('2021-01-01'),
+            createdAt: new Date('2021-01-01'),
+            closedAt: new Date('2021-01-02'),
+         },
+         { id: 3,
+            status: 'In Progress',
+            name: 'Issue 3',
+            description: 'Issue 3 description',
+            assignedTo: {id: 6, firstName: "Gabi", lastName: "Winkler"},
+            createdBy: {id: 4, firstName: "Bernd", lastName: "Mustermann"},
+            dueTo: new Date('2021-01-01'),
+            createdAt: new Date('2021-01-01'),
+            closedAt: new Date('2021-01-02'),
+         },
+        ],
+      };
+    store.addProject(project);
+
+    expect(store.projects).toHaveLength(1);
+    expect(store.projects[0]).toEqual(project);
+  });
+
+  test('should delete a project from the store', () => {
+    const project1 = {
+        id: 1,
+        name: 'Project 1',
+        description: 'Project 1 description',
+        milestones: [],
+        issues: [
+          { id: 1,
+            status: 'Open',
+            name: 'Issue 1',
+            description: 'Issue 1 description',
+            assignedTo: {id: 4, firstName: "Bernd", lastName: "Mustermann"},
+            createdBy: {id: 4, firstName: "Bernd", lastName: "Mustermann"},
+            dueTo: new Date('2021-01-01'),
+            createdAt: new Date('2021-01-01'),
+            closedAt: new Date('2021-01-02'),
+         },
+         { id: 3,
+            status: 'In Progress',
+            name: 'Issue 3',
+            description: 'Issue 3 description',
+            assignedTo: {id: 6, firstName: "Gabi", lastName: "Winkler"},
+            createdBy: {id: 4, firstName: "Bernd", lastName: "Mustermann"},
+            dueTo: new Date('2021-01-01'),
+            createdAt: new Date('2021-01-01'),
+            closedAt: new Date('2021-01-02'),
+         },
+        ],
+      };
+    const project2 = {
+        id: 2,
+        name: 'Project 2',
+        description: 'Project 2 description',
+        milestones: [],
+        issues: [
+            { id: 5,
+              status: 'Open',
+              name: 'Issue 5',
+              description: 'Issue 5 description',
+              assignedTo: {id: 7, firstName: "Franzi", lastName: "Siemens"},
+              createdBy: {id: 8, firstName: "Helmut", lastName: "Freiherr"},
+              dueTo: new Date('2021-01-01'),
+              createdAt: new Date('2021-01-01'),
+              closedAt: new Date('2021-01-02'),
+           },
+           { id: 3,
+              status: 'In Progress',
+              name: 'Issue 3',
+              description: 'Issue 3 description',
+              assignedTo: {id: 6, firstName: "Gabi", lastName: "Winkler"},
+              createdBy: {id: 4, firstName: "Bernd", lastName: "Mustermann"},
+              dueTo: new Date('2021-01-01'),
+              createdAt: new Date('2021-01-01'),
+              closedAt: new Date('2021-01-02'),
+           },
+          ],
+      };
+    store.addProject(project1);
+    store.addProject(project2);
+
+    expect(store.projects).toHaveLength(2);
+
+    store.deleteProject(project1.id);
+
+    expect(store.projects).toHaveLength(1);
+    expect(store.projects[0]).toEqual(project2);
+  });
+});
--- src/views/__tests__/EmployeeOverview.spec.ts
+++ src/views/__tests__/EmployeeOverview.spec.ts
@@ -16,6 +16,8 @@ import EmployeeOverview from '../EmployeeOverview.vue';
 
 const pinia = createPinia();
 describe('Employee Overview should load all the Components', () => {
+
+
   const wrapper = mount(EmployeeOverview, {
     global: {
       plugins: [PrimeVue, router, pinia],
@@ -43,5 +45,20 @@ describe('Employee Overview should load all the Components', () => {
     const multiselects = wrapper.findAllComponents(MultiSelect);
     expect(0).toEqual(multiselects[0].props('options').length);
     expect(6).toEqual(multiselects[1].props('options').length);
+  });
+
+  test('displays the correct title', () => {
+    const title = wrapper.find('.PageTitel');
+    expect(title.text()).toBe('Employee Overview');
+  });
+
+  test('Select a project and status on the project multiselector', async () => {
+    const multiselects = wrapper.findAllComponents(MultiSelect);
+    multiselects[1].setValue([multiselects[1].props('options')[2]]).finally(() => {
+      expect('Mocking Bird Project').toEqual(multiselects[1].find('.p-multiselect-label').text());
+      multiselects[0].setValue([multiselects[0].props('options')[0]]).finally(() => {
+        expect(13).toEqual(wrapper.getComponent(DataView).props('value').length);
+      });
+    });
   });
 });
