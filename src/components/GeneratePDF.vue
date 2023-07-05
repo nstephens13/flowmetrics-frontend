b0d5aa5422d22321f9fffeaae1a444b619bfd510
Estelle Wilfer <estelle_bianca.wilfer@stud.htwk-leipzig.de>
2023-07-05 18:44:55 +0200

    feat:#37 added SLA rules to PDF

--- src/components/GeneratePDF.vue
+++ src/components/GeneratePDF.vue
@@ -12,19 +12,24 @@ import type { SLARule } from '@/model/SLARule';
 
 const project = getMockData(7) as ProjectIF;
 const issues = project.issues as IssueIF[];
-const slaRule = issues.assignedSLARule as SLARule;
+const slaRulesSet: Set<SLARule> = new Set<SLARule>();
+
+issues.forEach((issue) => {
+  if (issue.assignedSLARule != null) {
+    slaRulesSet.add(issue.assignedSLARule);
+  }
+});
 
 const generatePDF = () => {
   // Create a new PDF
   const doc = new jsPDF();
   // Document title
-  doc.setFontSize(24);
   doc.text('SLA Rule Report', 10, 15);
+  doc.setFontSize(12);
   // Document content
-  project.issues.forEach((issue) => {
-    doc.setFontSize(18);
-    doc.text('SLA Rule 1', 10, 25);
-    doc.text({ issues }, 10, 30);
+  let y = 30;
+  slaRulesSet.forEach((slaRule) => {
+    doc.text(`SLA Rule ID: ${slaRule.id} Name: ${slaRule.name}`, 10, (y += 10));
   });
   doc.save('SLARuleReport.pdf');
 };
