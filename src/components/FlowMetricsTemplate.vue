1071cd2c3b761dd2f076a5766ebd210452f0066c
Estelle Wilfer <estelle_bianca.wilfer@stud.htwk-leipzig.de>
2023-10-25 20:05:03 +0200

    feat:#69 created FlowMetricsTemplate component

---
+++ src/components/FlowMetricsTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card" style="position: relative">
+    <Card>
+      <template #title>
+        Title
+        <Divider></Divider>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.p-card {
+  margin: 15px;
+  box-shadow: none;
+}
+</style>
