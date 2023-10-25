bac518147dfdb9ded52fa7333be1d3830d7bfe9e
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2023-10-25 20:01:32 +0200

    test:#54 Add tests for FilterConfigStore

---
+++ src/store/__tests__/FilterConfigStore.spec.ts
@@ -0,0 +1,27 @@
+import { describe, expect, test } from 'vitest';
+import { createPinia, setActivePinia } from 'pinia';
+import useFilterConfigStore from '@/store/FilterConfigStore';
+import type { ProjectIF } from '@/model/ProjectIF';
+import type { ProjectFilterConfigIF } from '@/model/FilterConfigIF';
+
+// Test Data
+const filterConfig1 = {
+  id: 1,
+  projectFilter: {
+    projectsWhiteList: [] as ProjectIF[],
+    issueStatusIncludeFilter: [] as string[],
+  } as ProjectFilterConfigIF,
+};
+
+describe('FilterConfigStore Tests', () => {
+  setActivePinia(createPinia());
+  test(' Filter id should be 1', () => {
+    const filterConfigStore = useFilterConfigStore();
+    expect(filterConfigStore.getFilterConfig.id).toEqual(1);
+  });
+  test('setFilterConfig and getFilterConfig', () => {
+    const filterConfigStore = useFilterConfigStore();
+    filterConfigStore.setFilterConfig(filterConfig1);
+    expect(filterConfigStore.getFilterConfig).toEqual(filterConfig1);
+  });
+});
