eb38e1aab0c4fd6d4ffce119130902e3e1f2fc9d
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2023-10-25 09:51:03 +0200

    refactor:#54 Add initialization for stores

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,10 @@ import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 
+import initSLAStore from './store/mockdata/initSLAStore';
+import initProjectStore from './store/mockdata/initProjectStore';
+import initFilterConfigStore from './store/mockdata/initFilterConfigStore';
+
 const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
@@ -61,3 +65,8 @@ app.component('AutoComplete', AutoComplete);
 app.component('Chip', Chip);
 
 app.mount('#app');
+
+// initialize the stores
+initSLAStore();
+initProjectStore();
+initFilterConfigStore();
