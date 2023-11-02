83a20b840771ad95fad605100002235aabeffdc9
iabi <ismail.abi@stud.htwk-leipzig.de>
2023-11-02 16:08:53 +0100

    fix:#67 issueStateFilter test

--- src/model/EmployeeIF.ts
+++ src/model/EmployeeIF.ts
@@ -4,6 +4,7 @@
  * @prop {string} firstName first name of the employee
  * @prop {string} lastName family name of the employee
  * @prop {string} emailAddress email address of the employee
+ * @prop {string} status the current status of the employee if is he still working or quit
  */
 export interface EmployeeIF {
   id: number;
