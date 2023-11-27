35e7649303d3a9928e452f7df67af7cf22cd07b1
ceggers <clara.eggers@stud.htwk-leipzig.de>
2023-11-27 20:35:07 +0100

    fix:#79 merging problems fix

--- src/services/__tests__/issueCalculator.spec.ts
+++ src/services/__tests__/issueCalculator.spec.ts
@@ -36,7 +36,7 @@ describe('Issue Calculator should map correctly ', () => {
     dueTo: null,
     assignedTo: testEmployee,
     statusChanges: null,
-    assignedSLARule: null,
+    assignedSlaRule: null,
     lastStatusChange: null,
   };
   const additionalProject: ProjectIF = {
