be92cb753c397d324347052c3f64e0dd7ec9e2c6
pfroehle <philip_joshua.froehle@stud.htwk-leipzig.de>
2023-12-19 14:34:16 +0100

    feat:#98 starting to impement

--- src/components/ProjectDescriptionPanel.vue
+++ src/components/ProjectDescriptionPanel.vue
@@ -27,6 +27,9 @@
       </template>
     </Card>
   </div>
+  <div>
+    <ZombieTicketCard :project="selectedProject"></ZombieTicketCard>
+  </div>
   <div class="card">
     <Card>
       <template #title>
@@ -114,6 +117,7 @@ import { getIssueStatusList, type ProjectIF } from '@/model/ProjectIF';
 import { calculateRemainingReactionTime, calculateStatusChanges } from '@/services/issueCalculator';
 import type { IssueIF } from '@/model/Issue/IssueIF';
 import projectStore from '@/store/projectStore';
+import ZombieTicketCard from "@/components/ZombieTicketCard.vue";
 
 // Create a reference for the selectedProject with initial data
 const selectedProject = ref({
---
+++ src/components/ZombieTicketCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <Card>
+      <template #title>Zombie Tickets</template>
+      <template #content>
+        <DataTable
+          paginator
+          :rows="10"
+          :rowsPerPageOptions="[10, 20, 50, 100]"
+          :value="filteredIssues"
+          showGridlines
+        >
+          <Column field="id" header="Issue ID"></Column>
+          <Column field="name" header="Name"></Column>
+          <Column field="assignedTo" header="Assigned to"></Column>
+          <Column field="createdAt" header="Created on"></Column>
+          <Column field="state" header="State"></Column>
+          <Column field="statusChanges.length" header="Status Changes"></Column>
+          <Column field="status" header="Status"></Column>
+          <Column field="statusRestingTime" header="Resting time (Status)"></Column>
+        </DataTable>
+      </template>
+    </Card>
+    <Card>
+      <template #content>
+        <div>Minimal resting time in current status (days)</div>
+        <div>Minimal number of status changes</div>
+        <div>
+          <Button>
+
+          </Button>
+          <Button>
+
+          </Button>
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ProjectIF } from '@/model/ProjectIF';
+import {Ref, ref} from "vue";
+import type {IssueIF} from "@/model/Issue/IssueIF";
+import useFilterConfigStore from "@/store/filterConfigStore";
+
+defineProps({
+  project: Object as () => ProjectIF,
+});
+const filteredIssues: Ref<[IssueIF]> = ref([]);
+const maximumRestingTime: Ref<number> = ref(0);
+const minimalStatusChanges: Ref<number> = ref(0);
+const filterConfigStore = useFilterConfigStore();
+filterConfigStore.setFilterConfig()
+filteredIssues.value =
+</script>
