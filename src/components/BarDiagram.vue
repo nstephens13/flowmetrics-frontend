279c05bfa845be71c29f4e31cdbdc24067f79899
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2023-12-21 23:02:51 +0100

    feat:#104 Refactor BarDiagram component and update chart options

--- src/components/BarDiagram.vue
+++ src/components/BarDiagram.vue
@@ -1,49 +1,47 @@
 <template>
   <card style="background-color: #ffffff; color: var(--flowMetricsBlue); width: 100%">
     <template #content>
-      <Chart type="bar" :data="chartData" :options="chartOptions" class="w-100rem h-20rem" />
+      <Chart type="bar" :data="chartData" :options="chartOptions" class="w-100rem h-30rem" />
     </template>
   </card>
 </template>
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import type { ProjectIF } from '@/model/ProjectIF';
-
-const chartData = ref();
-const chartOptions = ref();
+import { getStatusesfromIssues } from '@/services/issueCalculator';
 
 const documentStyle = getComputedStyle(document.documentElement);
 const textColor = documentStyle.getPropertyValue('--text-color');
 const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
 const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
-defineProps({
+const props = defineProps({
   project: {
     type: Object as () => ProjectIF,
     required: true,
   },
 });
-onMounted(() => {
-  chartData.value = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+
+const chartOptions = ref({});
+
+const chartData = computed(() => {
+  const labels = Array.from(getStatusesfromIssues(props.project.issues).keys());
+  const AllNumberOfIssues = Array.from(getStatusesfromIssues(props.project.issues).values());
+  return {
+    labels,
     datasets: [
       {
-        label: 'My First dataset',
+        label: 'Issues',
         backgroundColor: documentStyle.getPropertyValue('--blue-500'),
         borderColor: documentStyle.getPropertyValue('--blue-500'),
-        data: [65, 59, 80, 81, 56, 55, 40],
-      },
-      {
-        label: 'My Second dataset',
-        backgroundColor: documentStyle.getPropertyValue('--pink-500'),
-        borderColor: documentStyle.getPropertyValue('--pink-500'),
-        data: [28, 48, 40, 19, 86, 27, 90],
+        data: AllNumberOfIssues,
       },
     ],
   };
+});
+
+onMounted(() => {
   chartOptions.value = {
-    maintainAspectRatio: false,
-    aspectRatio: 0.8,
     plugins: {
       legend: {
         labels: {
@@ -55,22 +53,18 @@ onMounted(() => {
       x: {
         ticks: {
           color: textColorSecondary,
-          font: {
-            weight: 500,
-          },
         },
         grid: {
-          display: false,
-          drawBorder: false,
+          color: surfaceBorder,
         },
       },
       y: {
+        beginAtZero: true,
         ticks: {
           color: textColorSecondary,
         },
         grid: {
           color: surfaceBorder,
-          drawBorder: false,
         },
       },
     },
