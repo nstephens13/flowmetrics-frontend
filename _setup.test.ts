7ddbdd4b322fc94af6f1de8d2cf3c10de2a40b59
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2024-01-09 22:09:36 +0100

    fix:#103 Remove unused canvas setup code

--- _setup.test.ts
+++
@@ -1,6 +0,0 @@
-import { createCanvas } from 'canvas';
-
-// Mock HTMLCanvasElement
-(global as any).HTMLCanvasElement.prototype.getContext = function createCanvasFunction() {
-  return createCanvas(1, 1).getContext('2d');
-};
