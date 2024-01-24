2827f6d13f435e8c3e82588d0c6539183b745f42
Nived Stephen <nived.stephen@stud.htwk-leipzig.de>
2024-01-24 12:03:50 +0100

    feat:#118 added JSDoc to reactionTimeCalculator.ts

--- src/services/reactionTimeCalculator.ts
+++ src/services/reactionTimeCalculator.ts
@@ -2,6 +2,13 @@ import { DateTime, type DurationLikeObject } from 'luxon';
 import type { IssueIF } from '@/model/Issue/IssueIF';
 import type { CategoryIF } from '@/model/Sla/CategoryIF';
 
+/**
+ * Function to calculate the reaction time for an issue
+ * @param creationDate The date when the issue was created
+ * @param reactionTime The reaction time to be added to the creation date
+ * @returns The date when the issue should be reacted to
+ * @author Nived Stephen
+ */
 function calculateReactionTimeFromCreationDate(
   creationDate: Date,
   reactionTime: DurationLikeObject
@@ -9,6 +16,13 @@ function calculateReactionTimeFromCreationDate(
   return DateTime.fromJSDate(creationDate).plus(reactionTime);
 }
 
+/**
+ * Function to get the reaction time for an issue based on the category rules
+ * @param category Category to be checked
+ * @param issue Issue to be checked
+ * @returns The reaction time for the issue based on the category rules, or a message if no rule is found or the issue type is null
+ * @author Nived Stephen
+ */
 export default function getReactionTimeForIssue(category: CategoryIF, issue: IssueIF): string {
   if (issue.issueType !== null && category.rules.length > 0) {
     const ruleToBeAssigned = category.rules.find((rule) =>
